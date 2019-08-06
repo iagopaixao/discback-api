f97acf3a3fd49297f6c79f3fc46560bf7cdea3ba
Iago Paixão <iagosilva.si@gmail.com>
2019-08-06 18:49:16 -0300

    adds disc resource integration test

---
+++ src/test/java/br/com/beblue/discbackapi/disc/DiscResourceIntegrationTest.java
@@ -0,0 +1,54 @@
+package br.com.beblue.discbackapi.disc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DiscResourceIntegrationTest {
+
+  @Autowired private WebApplicationContext wac;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+  }
+
+  @Test
+  public void shouldReturnOneDisc_whenGetDiscIsCalled() throws Exception {
+    mockMvc
+        .perform(
+            get("/discs/{id}", 100)
+                .accept(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+  }
+
+  @Test
+  public void shouldReturnDiscCatalog_whenGetCatalogIsCalled() throws Exception {
+    mockMvc
+        .perform(
+            get("/discs/catalog" )
+                .accept(APPLICATION_JSON_UTF8)
+                .contentType(APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON));
+  }
+}
